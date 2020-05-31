0cf6e6b5c3afac07b745195dfb03f5d28195f4d2
minamAn <minamhunmin@gmail.com>
2020-05-31 16:49:56 +0900

    update router

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,13 +4,16 @@ import '../Styles/index.scss'
 import Main from '../Containers/Main';
 import TopADContainer from '../Containers/TopADContainer';
 import Header from '../Containers/Header';
+import { HashRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <TopADContainer />
-      <Header />
-      <Main />
+      <Router>
+        <Header />
+        <Main />
+      </Router>
     </div>
   );
 }
