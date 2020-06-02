358b79fe43e21965e674f4d1b36d8754d4cdaf03
Anminam <minamhunmin@gmail.com>
2020-06-02 10:53:57 +0900

    add category event

--- src/Components/AllCategory.tsx
+++ src/Components/AllCategory.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 const AllCategory = () => {
-    
+
+    const [isOn, setIsOn] = useState<Boolean>(true);
+
+    const handleMouseOver = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        setIsOn(true);
+    }
+    const handleMouseLeave = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        setIsOn(false);
+    }
+
 
     return (
         <div className="all-category">
-            <button type="button" />
+            {/* <button type="button" /> */}
+            <div className={`menu ${isOn ? 'on' : ''}`} onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}/>
         </div>
     )
 }
