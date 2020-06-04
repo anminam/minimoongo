352ba9819d8b659677b7aae4d60da061b2bf623e
minamAn <minamhunmin@gmail.com>
2020-06-04 21:19:22 +0900

    add footer

---
+++ src/View/Containers/Footer/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="sitemap-area">
+                <ul className="sitemap-area__list">
+                    <li>
+                        <a>이용약관</a>
+                    </li>
+                    <li>
+                        <a>회사소개</a>
+                    </li>
+                    <li>
+                        <a>이용약관</a>
+                    </li>
+                    <li>
+                        <a>개인정보처리방침</a>
+                    </li>
+                    <li>
+                        <a>고객센터</a>
+                    </li>
+                    <li>
+                        <a>협력사여러분</a>
+                    </li>
+                    <li>
+                        <a>제휴·제안</a>
+                    </li>
+                    <li>
+                        <a>광고문의</a>
+                    </li>
+                    <li>
+                        <a>채용정보</a>
+                    </li>
+                    <li>
+                        <a>서비스 전체보기</a>
+                    </li>
+                    
+                </ul>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
