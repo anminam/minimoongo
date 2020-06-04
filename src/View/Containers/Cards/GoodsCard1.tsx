2c3382594614684b2739287780e74212a71ad3d0
minamAn <minamhunmin@gmail.com>
2020-06-04 22:15:16 +0900

    split NavData for reducer

--- src/View/Containers/Cards/GoodsCard1.tsx
+++ src/View/Containers/Cards/GoodsCard1.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { IGoods } from 'Interfaces/IGoods';
+import { NavUtils } from 'Core/nav/utils';
 
-const GoodsCard1 = ({ name, subscript, img }:IGoods) => {
+const GoodsCard1 = ({ name, categoryId, img }:IGoods) => {
+
+    const categoryName = NavUtils.getCategoryName(categoryId);
+    
     return(
         <div className="card">
             <div className="card__cover">
@@ -11,7 +15,7 @@ const GoodsCard1 = ({ name, subscript, img }:IGoods) => {
             }
             </div>
             <div className="card__contents">
-                <p className="card__contents__category">{subscript}</p>
+                <p className="card__contents__category">[{categoryName}]</p>
                 <p className="card__contents__title">
                     <a href="/">{name}</a>
                 </p>
