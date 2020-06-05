06a09e9138da8c27db0d91fb8ebb34429e8eafb5
Anminam <minamhunmin@gmail.com>
2020-06-05 09:37:10 +0900

    update md

--- src/Pages/KorBook.tsx
+++ src/Pages/KorBook.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 const KorBook = () => {
     return (
-        <div className="container_lg">
-            한국책
+        <div className="container_lg page">
+            <div className="category-page">
+                <div className="category-page__category">
+                    여긴 카테고리
+                </div>
+                <div className="category-page__contents">
+                    여긴....
+                </div>
+
+            </div>
         </div>
     )
 }
