8869777067cb0e415ae4c2c4ed9dfd9424186bcd
minamAn <minamhunmin@gmail.com>
2020-06-07 00:31:55 +0900

    update lint

--- src/Components/AllCategoryMenuSub.tsx
+++ src/Components/AllCategoryMenuSub.tsx
@@ -1,70 +1,77 @@
-import React, { useState, useEffect } from 'react';
-import { ICategory, IMainCategoryId, ICategories } from 'Interfaces/ICategory';
-import { connect } from 'react-redux';
-import { ICategoryReducer } from 'Core/category/categoryData';
-import { Utils } from 'Core/Utils';
-import {NavUtils} from 'Core/nav/utils';
-import { categoryUtils } from 'Core/category/utils';
-
+import React, { useState, useEffect } from "react";
+import { IMainCategoryId, ICategories } from "Interfaces/ICategory";
+import { connect } from "react-redux";
+import { ICategoryReducer } from "Core/category/categoryData";
+import { Utils } from "Core/Utils";
+import { NavUtils } from "Core/nav/utils";
+import { categoryUtils } from "Core/category/utils";
 
 interface IAllCategoryMenuSub {
-    // title?: string;
-    navCategoryId?: IMainCategoryId;
-    categoryData: ICategoryReducer
+  // title?: string;
+  navCategoryId?: IMainCategoryId;
+  categoryData: ICategoryReducer;
 }
 
+const AllCategoryMenuSub = ({
+  categoryData,
+  navCategoryId,
+}: IAllCategoryMenuSub) => {
+  const [displayCategoryList, setDisplayCategoryList] = useState<
+    ICategories[]
+  >();
+  const [title, setTitle] = useState<string>("");
 
-const AllCategoryMenuSub = ({ categoryData, navCategoryId }:IAllCategoryMenuSub) => {
+  useEffect(() => {
+    if (navCategoryId) {
+      setTitle(NavUtils.getCategoryName(navCategoryId));
 
-    const [displayCategoryList, setDisplayCategoryList] = useState<ICategories[]>();
-    const [title, setTitle]  = useState<string>('');
-
-    useEffect(()=> {
-        if( navCategoryId ) {
-            setTitle(NavUtils.getCategoryName(navCategoryId));
-
-            const categoryObj = Utils.findObject(categoryData.categoryList, navCategoryId);
-            if (categoryObj) {
-                setDisplayCategoryList(categoryUtils.getDisplayCategoryList(categoryObj.list));
-            }
-        }
-
-    },[categoryData, navCategoryId]);
-    
-    return (
-        <div className={`sub-category`}>
-            <div className="title">
-                <a href={'/'}>{title}</a>
-            </div>
-            <div className={`sub-category__list_container`}>
-                {
-                    displayCategoryList &&
-                    displayCategoryList.map((item, i) => {
-                        return (
-                            <ul key={i} className="category">
-                                {
-                                    item.list.map((iitem, ii ) => {
-                                        return <li key={ii}>
-                                            <a href={iitem.id} className={`${iitem.isBig ? 'bold' : ''}`}>{iitem.displayName}</a>
-                                        </li>
-                                    })
-                                }
-                            </ul>
-                        )
-                    })
-                }
-            </div>
-        </div>
-    )
-}
-
-
-const mapStateToProps = (state:any) => {
-
-    const {category} = state;
-    return {
-        categoryData: category
+      const categoryObj = Utils.findObject(
+        categoryData.categoryList,
+        navCategoryId
+      );
+      if (categoryObj) {
+        setDisplayCategoryList(
+          categoryUtils.getDisplayCategoryList(categoryObj.list)
+        );
+      }
     }
-}
+  }, [categoryData, navCategoryId]);
+
+  return (
+    <div className={`sub-category`}>
+      <div className="title">
+        <a href={"/"}>{title}</a>
+      </div>
+      <div className={`sub-category__list_container`}>
+        {displayCategoryList &&
+          displayCategoryList.map((item, i) => {
+            return (
+              <ul key={i} className="category">
+                {item.list.map((iitem, ii) => {
+                  return (
+                    <li key={ii}>
+                      <a
+                        href={iitem.id}
+                        className={`${iitem.isBig ? "bold" : ""}`}
+                      >
+                        {iitem.displayName}
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state: any) => {
+  const { category } = state;
+  return {
+    categoryData: category,
+  };
+};
 
 export default connect(mapStateToProps)(AllCategoryMenuSub);
--- src/Interfaces/IGoods.ts
+++ src/Interfaces/IGoods.ts
@@ -1,19 +1,20 @@
 export type TGoods = IGoods | IBook;
 
 export interface IGoods {
-    id: string
-    name: string
-    subscript?: string
-    img: string
-    categoryId: string
-    subCategoryId?: string
+  id: string;
+  name: string;
+  subscript?: string;
+  img: string;
+  categoryId: string;
+  subCategoryId?: string;
 }
 
 export interface IBook extends IGoods {
-    author: string
-    publisher: string
-    price: number
-    sellPrice: number
-    productDate: string
-    // http://image.kyobobook.co.kr/images/book/large/175/l9791190382175.jpg
+  author: string;
+  publisher: string;
+  price: number;
+  sellPrice: number;
+  productDate: string;
+  rate?: string;
+  // http://image.kyobobook.co.kr/images/book/large/175/l9791190382175.jpg
 }
