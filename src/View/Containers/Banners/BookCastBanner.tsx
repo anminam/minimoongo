9eeea4693e52a4864fdbee274274bf84d84e7fa8
minamAn <minamhunmin@gmail.com>
2020-06-07 14:27:53 +0900

    add bookcastbanner

---
+++ src/View/Containers/Banners/BookCastBanner.tsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import "Styles/index.scss";
+
+const BookCastBanner = () => {
+  const [firstTitle, setFirstTitle] = useState<string>();
+  const [firstSubTitle, setFirstSubTitle] = useState<string>();
+
+  useEffect(() => {
+    setFirstTitle("[5컷만화] 독자를 위해 태어난 책");
+    setFirstSubTitle("좋은책의 재발견 teaser#2");
+  }, []);
+
+  return (
+    <div className="book-cast-banner banner">
+      <div className="title-container">
+        <h2>출판사 북캐스트</h2>
+        <p>거의 모든, 책 이야기</p>
+      </div>
+      <div className="title-and-img-container">
+        <div className="cover">
+          <img
+            src="http://image.kyobobook.co.kr/ink/images/prom/2020/bookcast/06/bn_08.jpg"
+            alt={firstTitle}
+          />
+        </div>
+        <div className="title">{firstTitle}</div>
+        <p className="sub-title">{firstSubTitle}</p>
+      </div>
+      <div className="other-list">
+        <ul>
+          <li>
+            <a href="/">[4컷만화] 난 눈이 마주쳤을 뿐</a>
+          </li>
+          <li>
+            <a href="/">즐겁게 시작하는 제로웨이스트</a>
+          </li>
+          <li>
+            <a href="/">잠자기 전 읽어볼까?</a>
+          </li>
+          <li>
+            <a href="/">봄을 누리지 못한 당신에게</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default BookCastBanner;
--- src/View/Containers/Welcomes/Welcome1.tsx
+++ src/View/Containers/Welcomes/Welcome1.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import WelcomeMainEvent from "./WelcomeMainEvent";
 import { mainEventList } from "Core/data";
 import WelcomeMainNavBanner from "../Banners/WelcomeMainNavBanner";
+import BookCastBanner from "../Banners/BookCastBanner";
 
 const Welcome1 = () => {
   return (
     <div>
       <div className="container_lg flex">
         <WelcomeMainEvent list={mainEventList} />
-        <WelcomeMainNavBanner />
+        <BookCastBanner />
       </div>
     </div>
   );
