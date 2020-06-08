bb70e8184fd4eabbf4dcce8c2a35e3005b03f397
Anminam <minamhunmin@gmail.com>
2020-06-08 16:53:47 +0900

    add top banner

--- src/View/Containers/HeaderBanner.tsx
+++ src/View/Containers/HeaderBanner.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const HeaderBanner = () => {
-    return (
-        <div className="header__banner">
-            배너광고
-        </div>
-    )
-}
+  return (
+    <div className="header__banner">
+      <ul>
+        <li>첫번째</li>
+        <li>두번째</li>
+        <li>세번째배너</li>
+      </ul>
+    </div>
+  );
+};
 
 export default HeaderBanner;
