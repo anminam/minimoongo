3f8d7187512a071f3aae23d8c39faab993cd9406
minamAn <minamhunmin@gmail.com>
2020-06-09 20:28:42 +0900

    update producpage

--- src/Pages/ProductPage.tsx
+++ src/Pages/ProductPage.tsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BookTop from "View/Containers/Products/Book/BookTop";
 
 interface IProductPage {
   id: string;
+  match?: any;
 }
 
-const ProductPage = ({ id }: IProductPage) => {
+const ProductPage = ({ id, match }: IProductPage) => {
+  const [pId, setPId] = useState<string>("");
+
+  useEffect(() => {
+    if (match) {
+      setPId(match.params.id);
+    }
+  }, [match]);
+
   return (
-    <div className="container_lg">
-      <BookTop id={id} />
+    <div className="product-page">
+      <div className="container_lg">
+        <BookTop id={pId} />
+      </div>
     </div>
   );
 };
