71c10dc4f20a058d5bdf99fbb4dc1f73e66e9717
Anminam <minamhunmin@gmail.com>
2020-06-10 17:01:11 +0900

    Updates

---
+++ precache-manifest.e38fbf078cde4deed8761cc9d2c97198.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307dce2630c209ca8ee35c6f910c6326",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00d66be1f862dd8dcce9",
+    "url": "/static/css/main.9ea6be04.chunk.css"
+  },
+  {
+    "revision": "6fe8de35658fec95c7f1",
+    "url": "/static/js/2.41fc6e7c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.41fc6e7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00d66be1f862dd8dcce9",
+    "url": "/static/js/main.1c012cfa.chunk.js"
+  },
+  {
+    "revision": "e96de60c3d86e535c3b5",
+    "url": "/static/js/runtime-main.ecd918ad.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/minimoongo/precache-manifest.7a0ac1b369c336ad40eb1c71dcda8ca8.js"
+  "/precache-manifest.e38fbf078cde4deed8761cc9d2c97198.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/minimoongo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
