eb06956392fe9ec08981a9f189347ad53fc1e6f2
minamAn <minamhunmin@gmail.com>
2020-06-14 21:05:38 +0900

    update book button

--- src/View/Containers/BookBuyButtons.tsx
+++ src/View/Containers/BookBuyButtons.tsx
@@ -4,10 +4,11 @@ import Button from "Components/Button";
 const BookBuyButtons = () => {
   return (
     <div>
+      <Button type="buy" text="장바구니 담기" />
       <Button type="buy" text="바로구매" />
-      <Button type="buy" text="바로구매" />
-      <Button type="buy" text="바로구매" />
-      <Button type="buy" text="바로구매" />
+      <Button type="buy" text="바로드림 주문" />
+      <Button type="buy" text="선물하기" />
+      <Button type="buy" text="보관함 담기" />
     </div>
   );
 };
