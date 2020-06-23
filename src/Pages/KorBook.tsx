3c97fde2ef76b59a665d544ea7a14d8ea2908c99
Anminam <minamhunmin@gmail.com>
2020-06-23 13:30:37 +0900

    connect korbook event

--- src/Pages/KorBook.tsx
+++ src/Pages/KorBook.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import NavCategory from "Components/NavCategory";
 import { IMainCategoryId } from "Interfaces/ICategory";
+import CategoryEventSlider from "Components/CategoryEventSlider";
+import { bookKorbookEvents } from "Core/data";
 
 const KorBook = () => {
   const [categoryId] = useState<IMainCategoryId>("korbook");
+
+  // 이걸 서버에서 할까??
+  const imgPath = "assets/events/welcomes/categorywelcom/korbook/2020_06_23/";
 
   return (
     <div className="container_lg page">
@@ -11,7 +16,13 @@ const KorBook = () => {
         <div className="category-page__category">
           <NavCategory navCategoryId={categoryId} />
         </div>
-        <div className="category-page__contents">컨텐츠</div>
+        <div className="category-page__contents">
+          <CategoryEventSlider
+            list={bookKorbookEvents}
+            imgPath={imgPath}
+            width={690}
+          />
+        </div>
       </div>
     </div>
   );
