094ffdaa806975e8853b100a47d89fc92b378308
Anminam <minamhunmin@gmail.com>
2020-06-25 09:57:38 +0900

    add slideBook

--- src/stories/Containers/13-EventBookCard.stories.tsx
+++ src/stories/Containers/13-EventBookCard.stories.tsx
@@ -4,23 +4,18 @@ import { CustomProvider } from "CustomProvider";
 import { bookKorbookEvents } from "Core/data";
 import CategoryEventSlider from "Components/CategoryEventSlider";
 import BookCardEvent from "View/Containers/Cards/BookCardEvent";
+import { ILink } from "Interfaces/ILink";
+import { korbookHaveSliderEvent } from "Core/events/data";
 
 export default {
   title: "containers/BookCardEvent",
   component: BookCardEvent,
 };
 
-const imgPath = "assets/events/welcomes/categorywelcom/korbook/2020_06_23/";
+const obj = korbookHaveSliderEvent.list[0];
+
 export const Main = () => {
   return CustomProvider(() => {
-    return (
-      <BookCardEvent
-        id={"asdf"}
-        src={"asdf"}
-        path={"asdf"}
-        displayName={"displayName"}
-        href={"asdf"}
-      />
-    );
+    return <BookCardEvent {...obj} />;
   });
 };
