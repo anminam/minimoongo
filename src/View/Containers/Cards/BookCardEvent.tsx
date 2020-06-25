4abef82a2a82505f84d5403c565c184bd6302949
Anminam <minamhunmin@gmail.com>
2020-06-25 09:57:52 +0900

    update book cardEvent

--- src/View/Containers/Cards/BookCardEvent.tsx
+++ src/View/Containers/Cards/BookCardEvent.tsx
@@ -3,18 +3,20 @@ import { ILink } from "Interfaces/ILink";
 import { Utils } from "Core/Utils";
 import { Link } from "react-router-dom";
 
-const BookCardEvent = ({ id, src, path }: ILink) => {
+const BookCardEvent = ({ id, src, path, displayName }: ILink) => {
   const link = Utils.getLinkOfProduct(id);
 
   const handleCardClick = (event: React.MouseEvent<HTMLDivElement>) => {
     window.location.href = link;
   };
 
+  const imgPath = `${path || ""}${src}`;
+
   return (
     <div className="event-book-card" onClick={handleCardClick}>
       <div className="event-book-card__cover">
-        <Link to="/">
-          <img src={src} alt="name" />
+        <Link to={path || "/"}>
+          <img src={imgPath} alt={displayName} />
         </Link>
       </div>
     </div>
